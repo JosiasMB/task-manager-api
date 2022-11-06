5d36768efe380cd3e399a3a9cf52623d1625176d
JosiasMB <securejmb@gmail.com>
2022-11-05 23:34:57 -0400

    Creating and testing an API. Also we created the routes to manage data.

---
+++ db/mongodb.js
@@ -0,0 +1,76 @@
+const { MongoClient } = require("mongodb");
+const connectionURL = "mongodb://localhost:27017";
+
+const dataBaseName = "task-manager";
+MongoClient.connect(connectionURL, (error, client) => {
+  if (error) {
+    return console.log("Error al conectar con la base de datos.");
+  }
+  console.log("Conectado!...");
+  const db = client.db(dataBaseName);
+
+  /************************* CRUD *************************/
+
+  // Insert
+  // db.collection("users").insertMany(
+  //   [
+  //     {
+  //       name: "Josias Matos",
+  //       email: "josias@gmail.com",
+  //       age: 25,
+  //     },
+  //     {
+  //       name: "Josias Matos Batista",
+  //       email: "josiasb@gmail.com",
+  //       age: 25,
+  //     },
+  //   ],
+  //   (error, result) => {
+  //     if (error) {
+  //       return console.log("No se puedo insertar.");
+  //     }
+  //     console.log(result);
+  //   }
+  // );
+
+  //Find
+  //   db.collection("users")
+  //     .find({ email: "joel@gmail.com" })
+  //     .toArray((error, result) => {
+  //       if (error) {
+  //         return console.log("Error");
+  //       }
+  //       console.log(result);
+  //     });
+
+  //update
+  // db.collection("users")
+  //   .updateOne(
+  //     {
+  //       _id: new ObjectId("6365ad1fda9b93c950e47379"),
+  //     },
+  //     {
+  //       $set: {
+  //         email: "josias@hotmail.com",
+  //       },
+  //     }
+  //   )
+  //   .then((result) => {
+  //     console.log(result);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+
+  // Delete
+  // db.collection("users")
+  //   .deleteOne({
+  //     email: "josias@gmail.com",
+  //   })
+  //   .then((result) => {
+  //     console.log(result);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+});
---
+++ src/index.js
@@ -0,0 +1,67 @@
+const { MongoClient, ObjectId } = require("mongodb");
+const connectionURL = "mongodb://localhost:27017";
+
+const dataBaseName = "task-manager";
+const express = require("express");
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+let users = [];
+
+// MongoClient
+const client = new MongoClient(connectionURL);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send(`<h1>Servidor corriendo en puerto ${port}</h1>`);
+});
+
+// Gets complete collection
+app.get("/users", async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db(dataBaseName);
+    users = await db.collection("users").find({}).toArray();
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.close();
+  }
+  res.status(200).send(users);
+});
+
+// Get only one user from API
+app.get("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  let user = {};
+  try {
+    await client.connect();
+    const db = client.db(dataBaseName);
+    user = await db.collection("users").findOne({ _id: new ObjectId(id) });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.close();
+  }
+  res.status(200).send(user);
+});
+
+// Delete user from API
+app.get("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    await client.connect();
+    const db = client.db(dataBaseName);
+    user = await db.collection("users").deleteOne({ _id: new ObjectId(id) });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.close();
+  }
+  res.status(200).send("<p>User was deleted correctly!...");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto http://localhost:${port}`);
+});
